7ad5bef0dc928bb49e80ca95196b556108634dd5
iMagnus506 <imagnus506@gmail.com>
2025-03-30 20:14:47 +0530

    Some changes

--- BackgroundServices/EmailService/sendOrderApprovalEmail.js
+++ BackgroundServices/EmailService/sendOrderApprovalEmail.js
@@ -1,48 +1,48 @@
-const ejs = require("ejs");
-const dotenv = require("dotenv");
-const sendMail = require("../helpers/sendMail");
-const Order = require("../models/Order");
+  const ejs = require("ejs");
+  const dotenv = require("dotenv");
+  const sendMail = require("../helpers/sendMail");
+  const Order = require("../models/Order");
 
-dotenv.config();
+  dotenv.config();
 
-const sendOrderApprovalEmail = async () => {
-  const orders = await Order.find({ status: 0 });
-  const RATE_PER_UNIT = 1300; // Updated to Rs. 1300 per unit
+  const sendOrderApprovalEmail = async () => {
+    const orders = await Order.find({ status: 0 });
+    const RATE_PER_UNIT = 1300; // Updated to Rs. 1300 per unit
 
-  if (orders.length > 0) {
-    for (let order of orders) {
-      // Calculate total cost
-      const totalCost = order.units * RATE_PER_UNIT;
+    if (orders.length > 0) {
+      for (let order of orders) {
+        // Calculate total cost
+        const totalCost = order.units * RATE_PER_UNIT;
 
-      ejs.renderFile(
-        "templates/BloodDonationOrderApproval.ejs",
-        {
-          name: order.name,
-          bloodType: order.bloodType,
-          units: order.units,
-          urgency: order.urgency,
-          ratePerUnit: RATE_PER_UNIT,
-          totalCost: totalCost
-        },
-        async (err, data) => {
-          let messageoption = {
-            from: process.env.EMAIL,
-            to: order.email,
-            subject: "Blood Order Request Approved - LifeSource",
-            html: data,
-          };
+        ejs.renderFile(
+          "templates/BloodDonationOrderApproval.ejs",
+          {
+            name: order.name,
+            bloodType: order.bloodType,
+            units: order.units,
+            urgency: order.urgency,
+            ratePerUnit: RATE_PER_UNIT,
+            totalCost: totalCost
+          },
+          async (err, data) => {
+            let messageoption = {
+              from: process.env.EMAIL,
+              to: order.email,
+              subject: "Blood Order Request Approved - LifeSource",
+              html: data,
+            };
 
-          try {
-            await sendMail(messageoption);
-            await Order.findByIdAndUpdate(order._id, { $set: { status: 1 } });
-            await Order.findByIdAndDelete(order._id);
-          } catch (error) {
-            console.log(err);
+            try {
+              await sendMail(messageoption);
+              await Order.findByIdAndUpdate(order._id, { $set: { status: 1 } });
+              await Order.findByIdAndDelete(order._id);
+            } catch (error) {
+              console.log(err);
+            }
           }
-        }
-      );
+        );
+      }
     }
-  }
-};
+  };
 
-module.exports = { sendOrderApprovalEmail };
+  module.exports = { sendOrderApprovalEmail };
